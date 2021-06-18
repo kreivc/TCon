848fa929e531d173359cb41bf8786cdfe747099e
Ricky <phangr28@gmail.com>
2021-06-18 23:19:50 +0700

    cat finished need more positioning all

--- public/JS/security.js
+++ public/JS/security.js
@@ -2,9 +2,10 @@
 var nowLocation = document.location;
 var check1 = nowLocation.pathname.includes("home");
 var check2 = nowLocation.pathname.includes("rating");
+var check2 = nowLocation.pathname.includes("chat");
 
 if(localStorage.getItem("userId") == null){
-    if(check1 || check2){
+    if(check1 || check2 || check3){
         window.open("index.html", "_self");
     }
 }
