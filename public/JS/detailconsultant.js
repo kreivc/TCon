df762e0bba31b3cddaaa244b13d0e9a50ee1323d
Brian Karnadi Japar <briankarnadi43@gmail.com>
2021-06-19 22:06:22 +0700

    update detailconsultant

--- public/JS/detailconsultant.js
+++ public/JS/detailconsultant.js
@@ -2,9 +2,29 @@ var userId = document.location.search.split("=")[1]
 console.log(userId);
 
 
+const dataConsult = document.querySelector(".slider");
+const Http = new XMLHttpRequest();
+const url='https://tcon-api.herokuapp.com/consultant/getall';
+Http.open('GET', url);
+Http.send();
+Http.onreadystatechange = () => {
+    if (Http.readyState == 4 && Http.status == 200) {
+        var consultData = JSON.parse(Http.responseText);
+        let temp= "";
+        for (var i = 0; i < consultData.consultants.length; i++) {
+            if(consultData.consultants[i].userId == userId){
+                document.getElementById("name").innerHTML = consultData.consultants[i].name;
+                break;
+            }
+        }
+    }
+}
+
+
 var data = JSON.stringify({
     "userId": userId
 });
+
 console.log(data);
 var xhr = new XMLHttpRequest();
 xhr.withCredentials = false;
@@ -16,10 +36,19 @@ xhr.setRequestHeader("Access-Control-Allow-Credentials", true)
 
 xhr.send(data);
 res = JSON.parse(xhr.responseText)
-stat = res["status"];
 console.log(res)
-if(stat){
-    var biodata = document.getElementById("bio");
-    console.log(res.details.bio)
-    biodata.innerHTML= res.details.bio;
-}
+
+var biodata = document.getElementById("bio");
+var experience = document.getElementById("experience");
+var honor = document.getElementById("honor");
+var education = document.getElementById("education");
+var rating = document.getElementById("rating");
+biodata.innerHTML= res.details.bio;
+education.innerHTML= res.details.education;
+experience.innerHTML= res.details.experience;
+honor.innerHTML= res.details.honor;
+rating.innerHTML= res.details.rating;
+
+
+console.log(nameConsultant);
+
