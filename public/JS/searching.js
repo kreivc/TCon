789bf578c86324f58ea517cc13c3ea4fed78b29f
Brian Karnadi Japar <briankarnadi43@gmail.com>
2021-06-19 19:20:02 +0700

    Searching Updated

--- public/JS/searching.js
+++ public/JS/searching.js
@@ -1,22 +1,67 @@
 $(document).ready(function(){
-    $('.customer-logos').slick({
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3600,
-        arrows: false,
-        dots: false,
-        pauseOnHover: false,
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 4
-            }
-        }, {
-            breakpoint: 520,
-            settings: {
-                slidesToShow: 3
-            }
-        }]
-    });
+  initializeSlick();
 });
+
+
+const dataConsult = document.querySelector(".slider");
+const Http = new XMLHttpRequest();
+const url='https://tcon-api.herokuapp.com/consultant/getall';
+Http.open('GET', url);
+Http.send();
+Http.onreadystatechange = () => {
+    if (Http.readyState == 4 && Http.status == 200) {
+        var consultData = JSON.parse(Http.responseText);
+        let temp= "";
+        for (var i = 0; i < consultData.consultants.length; i++) {
+            temp += profileConsultantData(consultData.consultants[i]);
+            // console.log(temp);
+            dataConsult.innerHTML = temp;
+        }
+        initializeSlick(false);
+    }
+}
+
+function profileConsultantData(data) {
+    return `<a href=""><div class="slide">
+                <div class="information">
+                    <div class="profile">
+                        <img src="assets/anonymus.jpg" alt="">
+                    </div>
+                    <div class="name">#${data.name}</div>
+                </div>
+                </div>
+            </a>`;
+  }
+
+  
+const SLICK_SLIDER_SETTINGS = {
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3600,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 4
+        }
+    }, {
+        breakpoint: 520,
+        settings: {
+            slidesToShow: 3
+        }
+    }]
+}
+
+function initializeSlick(init = true){
+    const slickSection = '.customer-logos'
+    if (init) {
+        $(slickSection).slick(SLICK_SLIDER_SETTINGS);
+        return
+    }
+    $(slickSection).slick('unslick'); 
+    $(slickSection).slick(SLICK_SLIDER_SETTINGS);
+}
+
